5a7db93312bfa02648358dfa269a87a31c19ad73
VladislavShishlyannikov <117915628+VladislavShishlyannikov@users.noreply.github.com>
2024-12-01 21:23:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -115,8 +115,6 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Редактор базы данных")
         self.setGeometry(100, 100, 1270, 720)
 
-
-        # db_path = "../3 LR/posts.db"
         db_path = "posts.db"
         self.db_path = db_path
         self.db = QSqlDatabase.addDatabase("QSQLITE")
